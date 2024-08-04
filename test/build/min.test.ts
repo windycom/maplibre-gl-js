1343c561e63497885d48c072f3fd0fa4121500ae
Jakub Pelc <jakub.pelc@email.cz>
2024-08-04 14:36:52 +0200

    Update build size

--- test/build/min.test.ts
+++ test/build/min.test.ts
@@ -36,7 +36,7 @@ describe('test min build', () => {
         const decreaseQuota = 4096;
 
         // feel free to update this value after you've checked that it has changed on purpose :-)
-        const expectedBytes = 874511;
+        const expectedBytes = 874295;
 
         expect(actualBytes).toBeLessThan(expectedBytes + increaseQuota);
         expect(actualBytes).toBeGreaterThan(expectedBytes - decreaseQuota);
